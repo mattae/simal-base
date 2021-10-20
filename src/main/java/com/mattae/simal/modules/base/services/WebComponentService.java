da31b32f86791acebddacabb916e913d271f094f
mattae <matthew.edor@gmail.com>
2021-10-20 22:08:10 +0100

    - Update entity modules

--- src/main/java/com/mattae/simal/modules/base/services/WebComponentService.java
+++ src/main/java/com/mattae/simal/modules/base/services/WebComponentService.java
@@ -1,15 +1,9 @@
 package com.mattae.simal.modules.base.services;
 
-import com.blazebit.persistence.CriteriaBuilder;
-import com.blazebit.persistence.CriteriaBuilderFactory;
-import com.blazebit.persistence.view.EntityViewManager;
-import com.blazebit.persistence.view.EntityViewSetting;
-import com.mattae.simal.modules.base.domain.entities.Individual;
 import com.mattae.simal.modules.base.domain.entities.Module;
 import com.mattae.simal.modules.base.domain.entities.WebComponent;
 import com.mattae.simal.modules.base.domain.repositories.ExposedComponentRepository;
 import com.mattae.simal.modules.base.domain.repositories.WebComponentRepository;
-import com.mattae.simal.modules.base.domain.views.IndividualView;
 import com.mattae.simal.modules.base.services.dto.ComponentDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +11,6 @@ import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -29,9 +21,6 @@ import java.util.stream.Collectors;
 public class WebComponentService {
     private final WebComponentRepository webComponentRepository;
     private final ExposedComponentRepository exposedComponentRepository;
-    private final List<EntityManager> entityManagers;
-    private final EntityViewManager evm;
-    private final CriteriaBuilderFactory cbf;
 
     @PostAuthorize("returnObject != null && returnObject.authorities.size() > 0 ? hasAnyAuthority(returnObject.authorities) : true ")
     public ComponentDTO getComponentById(UUID id) {
